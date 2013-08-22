2a459d6bc19e3aacb5223f680bad19ccddcaa5e9
Gerardo Bort <gerardobort@gmail.com>
2013-08-22 09:18:18 -0300

    regexp file cleanup

--- derive-regex.js
+++ derive-regex.js
@@ -55,97 +55,12 @@ console.log('tmpNode:', tmpNode);
                     if (replacements[symbol].hasOwnProperty(k))
                         tmpNode[k] = replacements[symbol][k];
                 }
+                tmpNode[k] = deriveExpr(tmpNode[k]);
 console.log('tmpNode:', tmpNode);
             }
 console.log('derivedN:', derivedNode);
 console.log('derivedF:', derivedFormula);
             node = derivedNode;
-/*
-            var fnName = node.fn.toString().match(/^function (\w+)\s*\(/)[1];
-            switch (fnName) {
-                case 'add': break;
-                case 'subtract': break;
-                case 'multiply':
-                    var a = _.extend({}, node.params[0]),
-                        b = _.extend({}, node.params[1]),
-                        Da = deriveExpr(_.extend({}, a)),
-                        Db = deriveExpr(_.extend({}, b));
-                    return new mathjs.expr.node.OperatorNode('+', mathjs.add, [
-                        new mathjs.expr.node.OperatorNode('*', mathjs.multiply, [ Da, b ]),
-                        new mathjs.expr.node.OperatorNode('*', mathjs.multiply, [ a, Db ]),
-                    ]);
-                case 'divide':
-                    var a = _.extend({}, node.params[0]),
-                        b = _.extend({}, node.params[1]),
-                        Da = deriveExpr(_.extend({}, a)),
-                        Db = deriveExpr(_.extend({}, b)),
-                        const2 = new mathjs.expr.node.ConstantNode(2);
-                    return new mathjs.expr.node.OperatorNode('/', mathjs.divide, [
-                        new mathjs.expr.node.OperatorNode('-', mathjs.subtract, [
-                            new mathjs.expr.node.OperatorNode('*', mathjs.multiply, [ Da, b ]),
-                            new mathjs.expr.node.OperatorNode('*', mathjs.multiply, [ a, Db ]),
-                        ]),
-                        new mathjs.expr.node.OperatorNode('^', mathjs.pow, [ b, const2 ])
-                    ]);
-                case 'pow':
-                    if (node.params[1].value) {
-                        var a = _.extend({}, node.params[0]),
-                            b = _.extend({}, node.params[1]),
-                            exp = new mathjs.expr.node.ConstantNode(b.eval()-1);
-                        return new mathjs.expr.node.OperatorNode('*', mathjs.multiply, [
-                            b,
-                            new mathjs.expr.node.OperatorNode('^', mathjs.pow, [ a, exp ])
-                        ]);
-                    } else {
-                        // TODO  (@see u^v)
-                    }
-                case 'log':
-                default:
-                    console.log('unhandled expression:', fnName);
-                    console.log(node)
-            }
-*/
-        }
-        if (node.object) {
-/*
-            switch (node.object.name) {
-                case 'log':
-                    var u = node.params[0];
-                    if (u.object || u.fn) {
-                        return new mathjs.expr.node.OperatorNode('*', mathjs.multiply, [
-                            new mathjs.expr.node.OperatorNode('/', mathjs.divide, [
-                                1, u
-                            ]),
-                            deriveExpr(_.extend({}, u))
-                        ]);
-                    } else {
-                        return new mathjs.expr.node.OperatorNode('/', mathjs.divide, [
-                            1, u
-                        ]);
-                    }
-                case 'sin':
-                    var n = _.extend({}, node),
-                        o = _.extend({}, node.object),
-                        param = _.extend({}, node.params[0]);
-                    o.name = 'cos';
-                    n.object = o;
-                    return new mathjs.expr.node.OperatorNode('*', mathjs.multiply, [
-                        n, deriveExpr(param)
-                    ]);
-                case 'cos':
-                    var n = _.extend({}, node),
-                        o = _.extend({}, node.object),
-                        param = _.extend({}, node.params[0]);
-                    o.name = 'sin';
-                    n.object = o;
-                    return new mathjs.expr.node.OperatorNode('*', mathjs.multiply, [
-                        -1,
-                        new mathjs.expr.node.OperatorNode('*', mathjs.multiply, [
-                            n, deriveExpr(param)
-                        ])
-                    ]);
-            }
-*/
         }
 
         if (!node.params) {
